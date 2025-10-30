6b00312a17597816ff8c6a88c1adbea8663bab81
Janajha-inwebb <janajha.howard@jeffersonrise.org>
2025-10-30 17:48:27 +0000

    message

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -15,7 +15,7 @@ $(function () {
       //start game
       setInterval(main, 1000 / frameRate);
     }
-
+  
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
     createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
@@ -27,36 +27,46 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    toggleGrid();
-
+    //toggleGrid();
+//I TURNED OFF GRID BECAUSE IT MAKES GAME EXTRA GLITCHY
 
     // TODO 2 - Create Platforms
-createPlatform(300,700,200,20, "red")
-createPlatform(500,600,200,20, "red")
-createPlatform(350, 500, 100,20, "red")
-createPlatform(250,450, 100, 20, "red")
-createPlatform(100,175,50, 325,"red")
-createPlatform(150,400,65,20)
-createPlatform(499,600,5,120,"red")
-createPlatform(150,215,65,20)
-createPlatform(300,320,65,20)
-createPlatform(488,200,100,20,"red")
-createPlatform(648,200,100,20,"red")
-createPlatform(588,350,75,10, "red")
-createPlatform(700,400,100,20,"red")
-createPlatform(750,100,5,100,"red")
-createPlatform(1000,300,100,20,"red")
-createPlatform(1000,500,100,20,"red")
-createPlatform()
+createPlatform(300,700,200,20, "#CD5D67")
+createPlatform(500,600,200,20, "#CD5D67")
+createPlatform(350, 500, 100,20, "#CD5D67")
+createPlatform(250,450, 100, 20, "#CD5D67")
+createPlatform(100,175,50, 325,"#CD5D67")
+createPlatform(150,400,65,20,"#CD5D67")
+createPlatform(499,600,5,120,"#CD5D67")
+createPlatform(150,215,65,20,"#CD5D67")
+createPlatform(300,320,65,20,"#CD5D67")
+createPlatform(488,200,100,20,"#CD5D67")
+createPlatform(648,200,100,20,"#CD5D67")
+createPlatform(588,350,75,10, "#CD5D67")
+createPlatform(700,400,100,20,"#CD5D67")
+createPlatform(747,100,5,120, "#CD5D67")
+createPlatform(1000,300,100,20,"#CD5D67")
+createPlatform(1000,500,100,20,"#CD5D67")
 
+
+createPlatform(1050,625,100,20, "#CD5D67")
+createPlatform(1200,200,100,20, "#CD5D67")
+createPlatform(750,200,100,20,"#CD5D67")
 
 
     // TODO 3 - Create Collectables
-
-
+createCollectable("steve", 100, 140);
+createCollectable("diamond",1235,160)
+createCollectable("database",773,160)
+createCollectable("grace", 500,700)
+// createCollectable()
+// createCollectable("Diamond", 130, 150)
 
     
     // TODO 4 - Create Cannons
+createCannon("right",750,3000);
+createCannon("right",245, 1100);
+createCannon("top",375, 2400);
 
 
     
